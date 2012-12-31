285fb59d103114f7b08ab743ac2b6eb1cdf94e8e
Thales de Brito Buarque <thales_30@hotmail.com>
2012-12-31 14:03:34 -0200

    Updating

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 jQuery(document).ready(function() {
-	$("header fieldset #botao").click(function(){
-		var salario = $("#salariobruto").val();
-		var salarioINSS = $("#discountINSS").val(salario+"")
-		alert(salarioINSS);
-	});
+	$("header form button ").click(function(){
+		var salario =parseFloat($("#salariobruto").val());
+		if (salario<=1174.86) {
+		var salarioINSS = parseFloat($("#discountINSS").val(salario-=salario*0.08+""))
+	}
+		else if(salario>1174.86 & salario<=1958.10){
+		var salarioINSS = parseFloat($("#discountINSS").val(salario-=salario*0.09+""))
+	}
+		else{
+		var salarioINSS = parseFloat($("#discountINSS").val(salario-=salario*0.11+""))	
+	}
+		if (salarioINSS<=1697.11) {
+		var salarioIR = parseFloat($("#discountIR").val(salarioINSS+""))
+	}
+
+    });
 });
